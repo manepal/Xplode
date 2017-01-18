bc19a295e124ccb760a4e3b31dcea62266ff0772
Mahesh Nepal <nepalmhs@gmail.com>
2017-01-18 20:01:11 +0545

    added networked player prototype.

---
+++ Assets/Scripts/BombController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+	// used for checking the collision with sidewise ground element
+	public LayerMask whatIsBlocker;
+	public Transform blockCheck;
+	
+	[Range(0.0f, 6.0f)]
+	public float moveSpeed = 3.0f;
+	public int direction = 1;
+
+	private Rigidbody2D rigidbody;
+	private bool isBlocked;
+
+
+	void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		isBlocked = Physics2D.Linecast(transform.position, blockCheck.position, whatIsBlocker);
+		if(isBlocked)
+		{
+			var localScale = transform.localScale;
+			localScale.x *= - 1;
+			transform.localScale = localScale;
+
+			direction *= -1;
+		}
+
+		rigidbody.velocity = new Vector2(moveSpeed * direction, rigidbody.velocity.y);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+	[Range(1.0f, 6.0f)]
+	public float moveSpeed = 3f;
+	public float jumpForce = 400f;
+
+	public Transform groundCheckStart;
+	public Transform groundCheckFinish;
+	public LayerMask whatIsGround;
+
+	private Rigidbody2D rigidbody;
+	private bool isGrounded = false;
+	private float vx;
+	private float vy;
+
+
+	void Update()
+	{
+		if(!isLocalPlayer)
+		{
+			return;
+		}
+
+		vx = Input.GetAxisRaw("Horizontal");
+		vy = rigidbody.velocity.y;
+
+		isGrounded = Physics2D.Linecast(groundCheckStart.position, groundCheckFinish.position, whatIsGround);
+
+		if(Input.GetButtonDown("Jump") && isGrounded)
+		{
+			DoJump();
+		}
+		else if(Input.GetButtonUp("Jump") && vy > 0f)
+		{
+			vy = 0;
+		}
+
+		rigidbody.velocity = new Vector2(vx * moveSpeed, vy);
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+		if(rigidbody == null)
+		{
+			Debug.LogError("Component Rigidbody2D is missing in gameObject!");
+		}
+
+		// color local player blue
+		GetComponent<SpriteRenderer>().color = Color.blue;
+	}
+
+	void DoJump()
+	{
+		vy = 0;
+		rigidbody.AddForce(new Vector2(0, jumpForce));
+	}
+}
